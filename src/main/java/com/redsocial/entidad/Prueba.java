344ece4b667b5ede29a8bb7ae312196d11c69226
joedca86 <joedca86@gmail.com>
2020-05-16 20:00:39 -0500

    Agregando getters and seters

--- src/main/java/com/redsocial/entidad/Prueba.java
+++ src/main/java/com/redsocial/entidad/Prueba.java
@@ -2,4 +2,25 @@ package com.redsocial.entidad;
 
 public class Prueba {
 	private String nombre;
+	private int numero1;
+	private int numero2;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNumero1() {
+		return numero1;
+	}
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
+	public int getNumero2() {
+		return numero2;
+	}
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+	
 }
